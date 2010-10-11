0546d8ef99b0c8e4791ad890b597900c0704b338
James Yu <james@scribd.com>
2010-10-10 23:28:58 -0700

    about page

--- config/routes.rb
+++ config/routes.rb
@@ -45,6 +45,8 @@ ActionController::Routing::Routes.draw do |map|
 
   # See how all your routes lay out with "rake routes"
 
+  map.about 'about/:action', :controller => 'about'
+
   # Install the default routes as the lowest priority.
   # Note: These default routes make all actions in every controller accessible via GET requests. You should
   # consider removing or commenting them out if you're using named routes and resources.
